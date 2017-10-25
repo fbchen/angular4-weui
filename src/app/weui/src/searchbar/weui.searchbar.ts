6972aa79783f65f020ea87cd6165d08d4ebf3b77
chenfubing <fbchen@fbchendeMacBook-Pro.local>
2017-10-25 10:19:17 +0800

    增加Subscription的unsubscribe()

--- src/app/weui/src/searchbar/weui.searchbar.ts
+++ src/app/weui/src/searchbar/weui.searchbar.ts
@@ -6,8 +6,9 @@
  * found in the LICENSE file.
  */
 
-import { Component, Input, Output, EventEmitter, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy, ElementRef, ViewChild } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
+import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 
@@ -32,7 +33,7 @@ import 'rxjs/add/operator/distinctUntilChanged';
         </div>
     `
 })
-export class WeUISearchBar implements OnInit {
+export class WeUISearchBar implements OnInit, OnDestroy {
 
     /**
      * @i18n
@@ -71,18 +72,25 @@ export class WeUISearchBar implements OnInit {
     public value = '';
 
     private searchTerms = new Subject<string>();
+    private _searchTermsChangesSubscription: Subscription;
 
     constructor() {
 
     }
 
     ngOnInit(): void {
-        this.searchTerms
+        this._searchTermsChangesSubscription = this.searchTerms
             .debounceTime(300)        // wait for 300ms pause in events
             .distinctUntilChanged()   // ignore if next search term is same as previous
             .subscribe((term: string) => {
                 this.search.emit(term);
             });
+    }
+
+    ngOnDestroy(): void {
+        if (this._searchTermsChangesSubscription) {
+            this._searchTermsChangesSubscription.unsubscribe();
+        }
     }
 
     doFocus(): void {
